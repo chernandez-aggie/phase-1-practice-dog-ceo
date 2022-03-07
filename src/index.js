7b80af28e408e269cd98c88e03d83a94facdbb5b
candacehernandez <chernandez.aggie@gmail.com>
2022-03-07 16:19:06 -0600

    commit tree 7c07d54da9bf2f78c65894a58c2ea0a14341791b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',() => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+    .then(response => response.json())
+
+})
+
+function createImgElement(imgUrl){
+    forEach(imgUrl){    
+    let img = document.createElement('img');
+    img.src = imgUrl;
+    document.bod
+    }
+}
